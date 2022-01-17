236a960ff053c0f1c1a4888c0baf707ed820b3d5
GilliHorowitz <93007529+GilliHorowitz@users.noreply.github.com>
2022-01-17 18:07:30 +0200

    Add files via upload
     working:
        -Fusion node (customer) receiving from 2 producers
        - direct exchange + feedback to producer

---
+++ fusion/fusion.py
@@ -0,0 +1,68 @@
+import pika
+import sys
+import os
+import time
+import json
+
+
+def main():
+    # establish a connection with RabbitMQ server
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # creating an exchange
+    exchange_name = 'fusion_exchange'
+    channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
+
+    # ---------------------------------------------------------------
+    # # creating recipient queue
+    # queue_name = 'w1'
+    # result = channel.queue_declare(queue=queue_name)
+    #
+    # # connect exchange and queue
+    # channel.queue_bind(exchange=exchange_name, queue=queue_name)
+
+    # create a message queue
+    producer1_queue_name = "working_producer_1"
+    producer1_channel = channel.queue_declare(queue=producer1_queue_name)  # message queue = sends work to workers
+    producer2_queue_name = "working_producer_2"
+    producer2_channel = channel.queue_declare(queue=producer2_queue_name)  # message queue = sends work to workers
+
+    # define a callback method for when receiving a message
+    def callback(ch, method, properties, body):
+        print(' [x] Received')
+        print(' [x] Converting into json file')
+        # convert message into json file and save it
+        msg = json.dumps(body.decode())
+        out_file = open('out.json', 'w')
+        out_file.write(msg)
+        out_file.close
+        print(' [x] Saved json file')
+
+        # sending feedback to producer
+        response = ' [v] worker1 is done'
+        ch.basic_publish(exchange=exchange_name, routing_key=properties.reply_to,
+                         properties=pika.BasicProperties(correlation_id=properties.correlation_id), body=str(response))
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)  # acknowledging getting the message
+        print(' [x] Done')
+
+    # receive messages from 'Try' queue
+    channel.basic_consume(queue=producer1_queue_name, on_message_callback=callback)
+    channel.basic_consume(queue=producer2_queue_name, on_message_callback=callback)
+
+    # start listening to messages
+    print(' [*] waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+# define never-ending loop that waits for data
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupt")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ fusion/producer2.py
@@ -0,0 +1,97 @@
+import uuid
+import pika
+import sys
+import time
+import os
+import json
+import numpy as np
+
+
+class Producer(object):
+    def __init__(self):
+        #############################################
+        # creates the queue used to deliver work to workers
+        #############################################
+
+        # establish a connection with RabbitMQ server
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+
+        # creating an exchange
+        self.exchange_name = 'fusion_exchange'
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
+
+        # create a message queue
+        self.message_queue_name = "working_producer_2"
+        result = self.channel.queue_declare(queue=self.message_queue_name)  # message queue = sends work to workers
+        self.fb_queue_name = result.method.queue
+
+        # connect exchange and message queue
+        self.channel.queue_bind(exchange=self.exchange_name, queue=self.message_queue_name)
+
+        # receive message
+        self.channel.basic_consume(queue=self.fb_queue_name, on_message_callback=self.fusion_response, auto_ack=True)
+
+    def fusion_response(self, ch, method, props, body):
+        #############################################
+        # on response arrival, if the sender is identified as the worker we are waiting for,
+        # push the response to the producer
+        # params: props -       message queue properties
+        #         body -        response content
+        #############################################
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def wait_for_feedback(self):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+
+        # setting the task as body of the message
+        file_name = str(input('enter name of json file: '))
+        with open(f'./{file_name}') as f:
+            task = str(json.load(f))
+        print(f' [x] Sent {task}')
+
+        # connecting to channel
+        self.corr_id = str(np.random.rand())
+        self.channel.basic_publish(exchange=self.exchange_name, routing_key=self.message_queue_name,
+                                   properties=pika.BasicProperties(reply_to=self.fb_queue_name,
+                                                                   correlation_id=self.corr_id),
+                                   body=task)
+
+        # waiting for response
+        while self.response is None:
+            self.connection.process_data_events()
+
+        time.sleep(10)
+        print(" [x] Got an answer")
+        # close connection
+        self.connection.close()
+        return self.response
+
+
+def main():
+    producer = Producer()
+    res = producer.wait_for_feedback()
+
+
+# define never-ending loop that waits for data
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupt")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
+
+
+
+
+
+
+
+
+
